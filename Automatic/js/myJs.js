aa4c6fe33ae7334633a0f12d79f2ded0e29fa33e
Umesh <Umeshdangrecha240@gmail.com>
2020-05-21 16:20:21 +0530

    automatic slides images

--- Automatic/js/myJs.js
+++ Automatic/js/myJs.js
@@ -1,31 +1,22 @@
-var slideIndex = 1;
-showSlides(slideIndex);
+var slideIndex = 0;
+showSlides();
 
-
-function showSlides(n){
-    console.log("show slide "+slideIndex+"\n");
+function showSlides(){
     var i;
     var slides = document.getElementsByClassName("slides");
     var dots = document.getElementsByClassName("dot");
 
-    if(n<1)
-        slideIndex = slides.length;
-    if(n>slides.length) 
-        slideIndex = 1;
-    for(i = 0 ; i<slides.length ;i++){
-        slides[i].style.display="none"
+    for(i = 0 ;i < slides.length ; i++){
+        slides[i].style.display="none";
         dots[i].className = dots[i].className.replace(" active","");
     }
+    if(slideIndex<1)
+    slideIndex = slides.length;
+    else if(slideIndex>slides.length){
+        slideIndex = 1;
+    }
     slides[slideIndex-1].style.display="block";
-    dots[slideIndex-1].className +=" active";
-
+    dots[slideIndex-1].className+=" active";
+    slideIndex++;
+    setTimeout(showSlides,2000);
 }
-
-function plusSlide(n){
-    
-    showSlides(slideIndex+=n);
-}
-function currentSlide(n){
-    showSlides(slideIndex = n);
-}
-
